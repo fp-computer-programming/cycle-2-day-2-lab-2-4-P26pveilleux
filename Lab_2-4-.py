76be94dcbfc99c89b059632c43fbb6dd843da703
P26pveilleux <144148685+P26pveilleux@users.noreply.github.com>
2023-09-19 13:51:00 -0400

    i followed directions

---
+++ Lab_2-4-.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2
+c = 3 
+x = 2 
+y = 5
+first  =  (3+4*x / (5)) 
+print (first)
+middle = 10*(y-5)*(a + b + c)*(a + b + c) / (x)
+print (middle)
+last = 9*(4 / x*x)*(9 + x ) / (y)
+print (last)
